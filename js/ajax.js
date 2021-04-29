72329175930a934ee1b9731f1fe7ec8f8bccb2be
Jere Vuola <vuolajere@gmail.com>
2021-04-29 19:28:58 +0300

    basic data for recipe page (ingredient amounts missing)

--- js/ajax.js
+++ js/ajax.js
@@ -1,7 +1,7 @@
-let myHeaders = new Headers();
+const myHeaders = new Headers();
 myHeaders.append("Cookie", "__cfduid=d49b963c9b1790102868fa8a468e6f9571619076712");
 
-let requestOptions = {
+const requestOptions = {
     method: 'GET',
     headers: myHeaders,
     redirect: 'follow'
@@ -11,32 +11,36 @@ const apiKey = "0b02b34a3d03459684b2e75f78070faf";
 const apiKey2 = "79edc81463084c748c02be93655d351f";
 const apiKey3 = "b126b486227f44c68a49b6779d33f513";
 const apiKey4 = "dcb55105e1cb45c9a317c95ee1ba12ab";
+
 const inputField = document.getElementById("inputField");
 const submit = document.getElementById("submit");
-const resultsContainer = document.getElementById("results");
+const resultsContainer = document.querySelector(".results");
+const recipeContainer = document.querySelector(".recipe");
+
+const itemContainer = document.querySelector(".item");
 
 const numberOfResults = 9;
 
 function submitField(evt) {
     resultsContainer.innerHTML = "";
-    search(inputField.value);
+    searchRecipe(inputField.value);
+
 }
 
-function search(query) {
-    fetch("https://api.spoonacular.com/recipes/complexSearch?query=" + query + "&number=" + numberOfResults + "&apiKey=" + apiKey, requestOptions)
+function searchRecipe(query) {
+    fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${query}&number=${numberOfResults}&apiKey=${apiKey}`, requestOptions)
         .then(response => response.json())
-        .then(result => naytaTulokset(result))
+        .then(result => showResults(result))
         .catch(error => console.log('error', error));
 
 }
 
-function naytaTulokset(result) {
+function showResults(result) {
 
-    console.log(result.totalResults);
-
+    // TODO change from innerHTML to DOM
     for (let i = 0; i < numberOfResults; i++) {
         resultsContainer.innerHTML +=
-            `<div class="item">
+            `<div class="item" id="` + result.results[i].id + `" onClick="onRecipeItemClick(this.id)">
             <img src="` + result.results[i].image + `" alt="` + result.results[i].title + `">
             <p>` + result.results[i].title + `</p>
             <br>
@@ -44,91 +48,69 @@ function naytaTulokset(result) {
     }
 }
 
+
+function onRecipeItemClick(id) {
+    resultsContainer.style.display = "none";
+    recipeContainer.style.display = "block";
+
+    fetch(`https://api.spoonacular.com/recipes/${id}/information?includeNutrition=false&apiKey=${apiKey}`, requestOptions)
+        .then(response => response.json())
+        .then(recipe => showRecipe(recipe))
+        .catch(error => console.log('error', error));
+}
+
+function showRecipe(recipe) {
+
+    // TODO, muuta DOM? 
+    // TODO, jos kuvaa ei löydy niin näytä placeholder
+
+
+
+
+    recipeContainer.innerHTML = `
+    <div class="recipe">
+    <div class="col-1">
+        <h1 class="title">` + recipe.title + `</h1>
+        <p class="readyInMinutes">` + recipe.readyInMinutes + ` minutes</p>
+        <p class="servings">` + recipe.servings + ` servings</p>
+    </div>
+
+    <div class="col-2">
+        <img class="image" src="` + recipe.image + `">
+    </div>
+
+    <div class="col-1">
+        <h2 class="ingredients-steps-title">Ainekset</h2>
+        `+ getIngredients(recipe) + `
+    </div>
+
+    <div class="col-2">
+        <h2 id="steps" class="ingredients-steps-title">Ohje</h2>
+        <ol>
+        `+ getInstructions(recipe) + `
+        </ol>
+    </div>
+    `;
+}
+
+function getIngredients(recipe) {
+    html = "";
+    for (let i = 0; i < recipe.extendedIngredients.length; i++) {
+        html += `<p>` + recipe.extendedIngredients[i].name + `</p>`;
+    }
+    return html;
+}
+
+function getInstructions(recipe) {
+    html = "";
+    for (let i = 0; i < recipe.analyzedInstructions[0].steps.length; i++) {
+        html += `<li>` + recipe.analyzedInstructions[0].steps[i].step + `</li>`;
+    }
+    return html;
+}
+
+
 submit.addEventListener("click", submitField);
-
-/*const apiurl = "http://api.tvmaze.com/search/shows?q=";
-
-let apiKysely;
-
-
-const hakunappi = document.getElementById("hakunappi");
-if (hakunappi == null) {
-    console.log("Debug: hakunappia ei löytynyt")
-} else {
-    console.log("Debug: hakunappi löytyi!");
-}
-const hakuKentta = document.getElementById("hakuteksti");
-const mainElement = document.querySelector("main");
-
-hakunappi.addEventListener('click', teeKysely);
-
-function teeKysely() {
-    let hakusana = hakuKentta.value;
-
-    apiKysely = apiurl + hakusana;
-    console.log("Lähetettävä kysely: " + apiKysely);
-    teeHaku(apiKysely);
-}
-
-function teeHaku(apiKysely)  {
-
-    fetch(apiKysely).then(function(response) {
-        return response.json();
-    }).then(function(json) {
-        naytaVastaus(json);
-    });
-}
-
-function naytaVastaus(jsonData) {
-
-    let kuvanOsoite, altArvo, kuvaus, genre;
-
-    if (jsonData.length == 0) {
-        mainElement.innerHTML = `<h3>Annetulla haulla ei tuloksia.</h3>`;
-    } else {
-        mainElement.innerHTML = `<h3>Löytyneet hakutulokset:</h3>`;
-    }
-
-    for (let i = 0; i < jsonData.length; i++) {
-
-        if (jsonData[i].show.summary != null) {
-            kuvaus = jsonData[i].show.summary;
-        } else {
-            kuvaus = 'Description not found';
-        }
-
-        if (jsonData[i].show.summary != null) {
-            genre = jsonData[i].show.genres;
-        } else {
-            genre = 'Genre details not found';
-        }
-
-        try {
-            kuvanOsoite = jsonData[i].show.image.medium;
-            altArvo = 'Haettu kuva';
-
-        } catch (error) {
-            kuvanOsoite = 'img/notFound.jpg';
-            altArvo = 'Kuvaa ei löytynyt';
-
-        }
-        mainElement.innerHTML += `
-        <article>
-           
-            <div class="picFormat">
-            <h4>${jsonData[i].show.name}</h4>
-                <img src="${kuvanOsoite}" alt="${altArvo}">
-                <div class="description">${kuvaus}</div>
-                <p>${genre}</p>
-                <div>
-                <a href="${jsonData[i].show.officialSite}"><p>Series Official Site</p></a>
-                </div>
-            </div>
-            
-        </article>`;
-    }
-}*/
-
 
 
 
